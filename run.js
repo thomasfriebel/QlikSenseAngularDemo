eefe2fdc53dc334cb79e14cc2be165eb026e5720
Thomas Friebel <thomasfriebel@gmail.com>
2014-09-24 10:10:11 +0200

    run command added

--- run.js
+++ run.js
@@ -1,3 +1,4 @@
 var connect = require('connect');
 var serveStatic = require('serve-static');
 connect().use(serveStatic(__dirname)).listen(8085);
+console.log("Qlik Sense Angular Demo Webserver now listening on port 8085")
